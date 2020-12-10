4e4c03b2c2e587c6a05fcfb57e687a36f63cda69
Madiwa Simon <mad.simon01@gmail.com>
2020-12-09 21:18:54 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,4 @@
-# # your code goes here
-# - First you need to create a person class that is initialized with a name that cannot be changed.
-# - Each instance of class `Person`should be able to remember their name
-# - Each instance of  class `Person` should start with $25 in their bank accounts
-# - Each instance of  class `Person` should start with eight happiness points
-# - Each instance of  class `Person` should start with eight hygiene points
-# - The happiness and hygiene points should be able to change, however the maximum and minimum points for both happiness and hygiene should be 10 and  0 respectively
-# - The amount in the bank account should also be able to change, though it has no max or min.
+
 require 'pry'
 
 class Person
@@ -17,4 +10,83 @@ class Person
         @happiness = 8
         @hygiene = 8
     end
+
+    def happiness
+        if @happiness < 0 
+            @happiness = 0
+        elsif @happiness > 10
+            @happiness = 10
+        else
+            @happiness
+        end
+    end
+
+    def hygiene
+        if  @hygiene < 0 
+            @hygiene = 0
+        elsif @hygiene > 10
+            @hygiene = 10
+        else
+            @hygiene
+        end
+    end
+
+   
+    # 4. The `work_out` method should increment the person's happiness by two points, decrease their hygiene by three points, and return the Queen lyrics, "♪ another one bites the dust ♫".
+    
+    # 5. The `call_friend` method should accept another instance of the Person class, or "friend". The method should increment the caller and the callee's happiness points by three. If Stella calls her friend Felix, the method should return "Hi Felix! It's Stella. How are you?"
+    
+    # 6. Finally, the `start_conversation` method should accept two arguments, the person to start a conversation with and the topic of conversation.
+    
+    #   - If the topic is politics, both people get sadder and the method returns "blah blah partisan blah lobbyist".
+    #   - If the topic is weather, both people get a little happier and the method returns "blah blah sun blah rain".
+    #   - If the topic is not politics or weather, their happiness points don't change and the method returns "blah blah blah blah blah".
+    
+    def happy?
+        @happiness > 7
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def get_paid (salary)
+        @bank_account += salary
+        "all about the benjamins"
+    end
+    
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend (person)
+        self.happiness += 3
+        person.happiness += 3
+       "Hi #{person.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation (friend, topic)
+        # binding.pry
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
 end
+
+# binding.pry
+# 0
